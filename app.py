cf8f45f0f0acc9909da99dc29a92a44001045acf
H-mD <syahrizalpmg@gmail.com>
2024-03-31 11:35:46 +0700

    feat: add main script

---
+++ app.py
@@ -0,0 +1,81 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+url = os.environ.get("URL")
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.environ.get("CREDENTIALS_FILE"), scope)
+client = gspread.authorize(creds)
+
+classes = ["A", "B", "C", "D", "E", "F", "IUP"]
+modul = 1
+type = "praktikum"
+header = 2
+side = 3
+
+if type == "praktikum":
+    contest = "PRAKTIKUM"
+    n = 0
+    r = 4
+elif type == "remidi":
+    contest = "REMIDI"
+    n = 4
+    r = 4
+elif type == "revisi":
+    contest = "REVISI"
+    n = 8
+    r = 3
+else:
+    print("Invalid type!")
+    exit()
+
+data = {}
+for kelas in classes:
+    api = f'https://www.its.ac.id/informatika/domjudge/api/v4/contests/{contest}-{kelas}-MODUL{modul}/scoreboard?strict=false'
+    response = requests.get(api)
+    if response.status_code == 200:
+        data[kelas] = response.json()
+    else:
+        print('Error:', response.status_code)
+        data[kelas] = None
+
+sheet = client.open_by_url(url)
+worksheet = sheet.worksheet("REKAP")
+kelas_col = worksheet.col_values(3)
+nrp_col = worksheet.col_values(1)
+
+value = []
+row = 0
+for kelas in classes:
+    class_data = data[kelas]
+    if class_data is None:
+        break
+    
+    df = pd.DataFrame(data[kelas]['rows'])
+    
+    for nrp_cell, kelas_cell in zip(nrp_col[header+row:], kelas_col[header+row:]):
+        if kelas_cell != kelas:
+            break
+
+        team = df[df['team_id'] == nrp_cell]
+        solved = [0] * r
+
+        if not team.empty:
+            problems = team.iloc[0]['problems']
+            for i, problem in enumerate(problems):
+                if problem['solved']:
+                    solved[i] = 1
+            value.append(solved)
+        else:
+            print("Praktikan tidak ditemukan!")
+            value.append(solved)
+
+        row += 1
+
+if row > 0:
+    range = f"{chr(side + (1 + ((modul-1)*11)) + n + 96)}{header+1}:{chr(side + (1 + ((modul-1)*11)) + n + (r-1) + 96)}{header + row}"
+    worksheet.update(value, range)
